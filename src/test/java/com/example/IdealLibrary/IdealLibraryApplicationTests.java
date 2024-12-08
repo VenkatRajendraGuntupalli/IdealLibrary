cbaf51c395aa1c29d3abedec96a72ab8ca802843
gvrgvrgvr <venkat.rajendra22@gmail.com>
2024-12-08 00:04:52 -0600

    Initial commit

---
+++ src/test/java/com/example/IdealLibrary/IdealLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IdealLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
